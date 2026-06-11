07cb327f0ad32306078bbe78263c855cf702f936
Piotr Nusbaum <piotr.nusbaum@mediaport.pl>
2026-06-11 08:40:28 +0200

    Trying to solve problem with db connection

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -19,3 +19,32 @@ resource "google_artifact_registry_repository" "docker_repo" {
   description   = "Docker images"
   format        = "DOCKER"
 }
+
+resource "google_cloud_run_v2_service" "product_api" {
+  name                = "product-api"
+  location            = var.region
+  deletion_protection = false
+  template {
+    containers {
+      image = "europe-central2-docker.pkg.dev/${var.project_id}/docker-images/product-api:latest"
+
+      env {
+        name  = "DATABASE_URL"
+        value = "postgresql+psycopg://dummy:dummy@localhost:5432/dummy"
+      }
+
+    }
+  }
+
+  depends_on = [
+    google_project_service.services
+  ]
+}
+
+resource "google_cloud_run_v2_service_iam_member" "public" {
+  location = google_cloud_run_v2_service.product_api.location
+  name     = google_cloud_run_v2_service.product_api.name
+
+  role   = "roles/run.invoker"
+  member = "allUsers"
+}
